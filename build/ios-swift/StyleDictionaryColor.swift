c61c60395868ba050ea75615467bd6efd61dfe82
Automated Publisher <actions@users.noreply.github.com>
2021-05-02 12:02:06 +0000

    Automated publish: Sun May  2 12:02:06 UTC 2021 5ca49be851a8df50b11eeaf56be3706079a42126

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -3,18 +3,49 @@
 // StyleDictionaryColor.swift
 //
 // Do not edit directly
-// Generated on Sun, 02 May 2021 11:49:35 GMT
+// Generated on Sun, 02 May 2021 12:02:05 GMT
 //
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let baseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha:1)
-    public static let baseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha:1)
-    public static let baseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha:1)
-    public static let baseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha:1)
-    public static let baseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha:1)
-    public static let fontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha:1)
-    public static let fontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha:1)
-    public static let fontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha:1)
+    public static let  = rgba(0, 0, 0, 1)
+    public static let color = rgba(0, 0, 0, 0.05)
+    public static let color = rgba(0, 0, 0, 0.04)
+    public static let color = rgba(0, 0, 0, 0.04)
+    public static let 100 = rgba(251, 239, 239, 1)
+    public static let 200 = rgba(243, 204, 204, 1)
+    public static let 300 = rgba(243, 172, 172, 1)
+    public static let 400 = rgba(229, 128, 128, 1)
+    public static let 500 = rgba(204, 0, 0, 1)
+    public static let 600 = rgba(153, 0, 0, 1)
+    public static let 100 = rgba(247, 247, 252, 1)
+    public static let 200 = rgba(239, 240, 246, 1)
+    public static let 300 = rgba(217, 219, 233, 1)
+    public static let 400 = rgba(160, 163, 189, 1)
+    public static let 50 = rgba(247, 247, 252, 1)
+    public static let 500 = rgba(110, 113, 145, 1)
+    public static let 600 = rgba(78, 75, 102, 1)
+    public static let 700 = rgba(50, 47, 70, 1)
+    public static let 800 = rgba(20, 20, 43, 1)
+    public static let 100 = rgba(229, 248, 255, 1)
+    public static let 200 = rgba(208, 232, 245, 1)
+    public static let 300 = rgba(162, 209, 235, 1)
+    public static let 400 = rgba(69, 163, 216, 1)
+    public static let 500Default = rgba(0, 115, 178, 1)
+    public static let 600 = rgba(3, 80, 123, 1)
+    public static let 100 = rgba(237, 246, 244, 1)
+    public static let 200 = rgba(209, 245, 238, 1)
+    public static let 300 = rgba(152, 210, 198, 1)
+    public static let 400 = rgba(114, 190, 174, 1)
+    public static let 500 = rgba(0, 153, 119, 1)
+    public static let 600 = rgba(0, 128, 93, 1)
+    public static let  = rgba(0, 0, 1, 1)
+    public static let 100 = rgba(251, 243, 226, 1)
+    public static let 200 = rgba(252, 234, 198, 1)
+    public static let 300 = rgba(253, 225, 169, 1)
+    public static let 400 = rgba(229, 194, 121, 1)
+    public static let 500 = rgba(226, 162, 31, 1)
+    public static let 600 = rgba(181, 129, 23, 1)
+    public static let  = rgba(255, 255, 255, 1)
 }
--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -3,21 +3,52 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Sun, 02 May 2021 11:49:35 GMT
+// Generated on Sun, 02 May 2021 12:02:05 GMT
 //
 
 #import <UIKit/UIKit.h>
 
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-ColorBaseGrayLight,
-ColorBaseGrayMedium,
-ColorBaseGrayDark,
-ColorBaseRed,
-ColorBaseGreen,
-ColorFontBase,
-ColorFontSecondary,
-ColorFontTertiary
+Black,
+White,
+Test,
+Grayscale50,
+Grayscale100,
+Grayscale200,
+Grayscale300,
+Grayscale400,
+Grayscale500,
+Grayscale600,
+Grayscale700,
+Grayscale800,
+Primary100,
+Primary200,
+Primary300,
+Primary400,
+Primary600,
+Primary500Default,
+Success100,
+Success200,
+Success300,
+Success400,
+Success500,
+Success600,
+Warning100,
+Warning200,
+Warning300,
+Warning400,
+Warning500,
+Warning600,
+Error100,
+Error200,
+Error300,
+Error400,
+Error500,
+Error600,
+DropShadowLargeColor,
+DropShadowMediumColor,
+DropShadowSmallColor
 };
 
 @interface StyleDictionaryColor : NSObject

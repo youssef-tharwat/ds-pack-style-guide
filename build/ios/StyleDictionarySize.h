5ca49be851a8df50b11eeaf56be3706079a42126
Youssef <yt@papershift.com>
2021-05-02 13:56:50 +0200

    first commit

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Sun, 02 May 2021 11:49:35 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
